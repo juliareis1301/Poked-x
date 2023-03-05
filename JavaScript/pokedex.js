42cbbc78b587998e520dc5a176156c3df805acc7
Julia Reis <juliadosreissilva6@gmail.com>
2023-03-04 23:10:01 -0300

    descrição do commit

--- JavaScript/pokedex.js
+++ JavaScript/pokedex.js
@@ -1,3 +1,105 @@
-function abrirCard () {
+
+function abrirCard1 () {
+  window.location.href = "bulbasaur.html";
+};
+
+function abrirCard2 () {
+  window.location.href = "charmander.html";
+};
+
+function abrirCard3 () {
+  window.location.href = "Squirtle.html";
+};
+
+function abrirCard4 () {
+  window.location.href = "Butterfree.html";
+};
+
+function abrirCard5 () {
+  window.location.href = "Pikachu.html";
+};
+
+function abrirCard6 () {
+  window.location.href = "Gastly.html";
+};
+
+function abrirCard7 () {
+  window.location.href = "Ditto.html";
+};
+
+function abrirCard8 () {
+  window.location.href = "Mew.html";
+};
+
+function abrirCard9 () {
+  window.location.href = "Aron.html";
+};
+
+function back () {
+  window.location.href = "index.html";
+}
+
+function next () {
+  window.location.href = "charmander.html";
+}
+
+function next2 () {
+  window.location.href = "Squirtle.html";
+}
+
+function next3 () {
+  window.location.href = "Butterfree.html";
+}
+
+function next4 () {
+  window.location.href = "Pikachu.html";
+}
+
+function next5 () {
+  window.location.href = "Gastly.html";
+}
+
+function next6 () {
+  window.location.href = "Ditto.html";
+}
+
+function next7 () {
+  window.location.href = "Mew.html";
+}
+
+function next8 () {
+  window.location.href = "Aron.html";
+}
+
+function backcard () {
   window.location.href = "bulbasaur.html";
 }
+
+function backcard2 () {
+  window.location.href = "charmander.html";
+}
+
+function backcard3 () {
+  window.location.href = "Squirtle.html";
+}
+
+function backcard4 () {
+  window.location.href = "Butterfree.html";
+}
+
+function backcard5 () {
+  window.location.href = "Pikachu.html";
+}
+
+function backcard6 () {
+  window.location.href = "Gastly.html";
+}
+
+function backcard7 () {
+  window.location.href = "Ditto.html";
+}
+
+function backcard8 () {
+  window.location.href = "Mew.html";
+}
+
