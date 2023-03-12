1b15a58f92f2c29eaef34a9497faff61479b6a54
Julia Reis <juliadosreissilva6@gmail.com>
2023-03-12 01:36:15 -0300

    descrição do commit

--- JavaScript/pokedex.js
+++ JavaScript/pokedex.js
@@ -1,111 +1,24 @@
-
-function abrirCard1 () {
-  window.location.href = "bulbasaur.html";
-};
-
-function abrirCard2 () {
-  window.location.href = "charmander.html";
-};
-
-function abrirCard3 () {
-  window.location.href = "Squirtle.html";
-};
-
-function abrirCard4 () {
-  window.location.href = "Butterfree.html";
-};
-
-function abrirCard5 () {
-  window.location.href = "Pikachu.html";
-};
-
-function abrirCard6 () {
-  window.location.href = "Gastly.html";
-};
-
-function abrirCard7 () {
-  window.location.href = "Ditto.html";
-};
-
-function abrirCard8 () {
-  window.location.href = "Mew.html";
-};
-
-function abrirCard9 () {
-  window.location.href = "Aron.html";
-};
-
-function back () {
-  window.location.href = "index.html";
+function abrirCard (pageName) {
+  window.location.href = pageName + ".html";
+}
+function next (pageName) {
+  window.location.href = pageName + ".html";
 }
 
-function next () {
-  window.location.href = "charmander.html";
+function back (pageName) {
+  window.location.href = pageName + ".html";
 }
 
-function next2 () {
-  window.location.href = "Squirtle.html";
-}
-
-function next3 () {
-  window.location.href = "Butterfree.html";
-}
-
-function next4 () {
-  window.location.href = "Pikachu.html";
-}
-
-function next5 () {
-  window.location.href = "Gastly.html";
-}
-
-function next6 () {
-  window.location.href = "Ditto.html";
-}
-
-function next7 () {
-  window.location.href = "Mew.html";
-}
-
-function next8 () {
-  window.location.href = "Aron.html";
-}
-
-function backcard () {
-  window.location.href = "bulbasaur.html";
-}
-
-function backcard2 () {
-  window.location.href = "charmander.html";
-}
-
-function backcard3 () {
-  window.location.href = "Squirtle.html";
-}
-
-function backcard4 () {
-  window.location.href = "Butterfree.html";
-}
-
-function backcard5 () {
-  window.location.href = "Pikachu.html";
-}
-
-function backcard6 () {
-  window.location.href = "Gastly.html";
-}
-
-function backcard7 () {
-  window.location.href = "Ditto.html";
-}
-
-function backcard8 () {
-  window.location.href = "Mew.html";
-}
-
-//ESCONDER "PESQUISAR"
-
-
-function esconderCampoPesquisa() {
-  this.style.display = 'none';
-}
+const pokemons = [
+  {
+      name: "Gastly",
+      type: ["ghost", "poison"],
+      weight: '40kg',
+      height: '1cm',
+      description: 'asdas',
+      movies: ['move 1', 'move 2']
+  },
+  {
+    name: "Pikachu"
+  }
+]
