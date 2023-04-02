d380359b688fa6b7e4256469fbf7eb567ed8cdde
Julia R <juliadosreissilva6@gmail.com>
2023-04-02 14:27:54 -0300

    feat: add html to pokemons page

--- JavaScript/pokedex.js
+++ JavaScript/pokedex.js
@@ -9,6 +9,7 @@ function back (pageName) {
   window.location.href = pageName + ".html";
 }
 
+
 const pokemons = [
   {
       name: "Gastly",
@@ -19,6 +20,67 @@ const pokemons = [
       movies: ['move 1', 'move 2']
   },
   {
-    name: "Pikachu"
-  }
+    name: "Bulbasaur",
+    type: ["Grass", "Poison"],
+    weight:'6,9kg',
+    height:'0,7cm',
+    description: 'Cholorophyll Overgrow',
+    movies: [''],
+  },
+  {
+    name:"Charmander",
+    type: ["fire"],
+    weight: '8,5kg',
+    height: '0,6m',
+    description: 'Mega-Punch Fire-Punch',
+    movies:[''],
+  },
+  {
+    name:"Squirtle",
+    type:["water"],
+    weight: '9,0kg',
+    height: '0,5m',
+    description: 'Torrent Rain-Dish',
+    movies:[''],
+  },
+  {
+    name:"Butterfree",
+    type:["bug", "flying"],
+    weight: '32kg',
+    height:'1,1m',
+    description: 'Compound-Eyes Tinted-Lens',
+    movies:[''],
+  },
+  {
+    name:"Pikachu",
+    type:["eletric"],
+    weight:'6,0kg',
+    height:'0,4m',
+    description: 'Mega-Punch Pay-Day',
+    movies:[''],
+  },
+  {
+    name:"Ditto",
+    type:["Normal"],
+    weight:'4,0kg',
+    height:'0,3m',
+    description: 'Limber Imposter',
+    movies:[''],
+  },
+  {
+    name: "Mew",
+    type:["Psychic"],
+    weight:'4kg',
+    height:'0,4m',
+    description: 'Synchronize',
+    movies:[''],
+  },
+  {
+    name:"Aron",
+    type:["steel", "rock"],
+    weight:'60kg',
+    height:'0,4m',
+    description:'Stur Rock-Head',
+    movies: [''],
+  },
 ]
